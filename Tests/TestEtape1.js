f22cd1423d1bba144ee04f204c5d2adf9be762ef
pierredepeyrot <pierredepeyrot@orange.fr>
2014-04-15 13:09:59 +0200

    uncomment

--- Tests/TestEtape1.js
+++ Tests/TestEtape1.js
@@ -5,7 +5,7 @@ QUnit.config.reorder = false;
 var query = new QueryAPI();
 
 QUnit.config.reorder = false;
-/*
+
 function test1(){
   var result = query.explore(new Array());
   var props = Object.keys(result);
@@ -98,17 +98,17 @@ function test11(){
   notEqual(result["data"], null);
   deepEqual(result["data"], {"[Zone.Name].[All Zone.Names].[Croatia]":{"caption":"Croatia"},"[Zone.Name].[All Zone.Names].[Switzerland]":{"caption":"Switzerland"},"[Zone.Name].[All Zone.Names].[Cyprus]":{"caption":"Cyprus"},"[Zone.Name].[All Zone.Names].[Portugal]":{"caption":"Portugal"},"[Zone.Name].[All Zone.Names].[France]":{"caption":"France"},"[Zone.Name].[All Zone.Names].[Italy]":{"caption":"Italy"},"[Zone.Name].[All Zone.Names].[United Kingdom]":{"caption":"United Kingdom"},"[Zone.Name].[All Zone.Names].[Finland]":{"caption":"Finland"},"[Zone.Name].[All Zone.Names].[Iceland]":{"caption":"Iceland"},"[Zone.Name].[All Zone.Names].[Slovakia]":{"caption":"Slovakia"},"[Zone.Name].[All Zone.Names].[Belgium]":{"caption":"Belgium"},"[Zone.Name].[All Zone.Names].[Luxembourg]":{"caption":"Luxembourg"},"[Zone.Name].[All Zone.Names].[Turkey]":{"caption":"Turkey"},"[Zone.Name].[All Zone.Names].[Norway]":{"caption":"Norway"},"[Zone.Name].[All Zone.Names].[Slovenia]":{"caption":"Slovenia"},"[Zone.Name].[All Zone.Names].[Austria]":{"caption":"Austria"},"[Zone.Name].[All Zone.Names].[Romania]":{"caption":"Romania"},"[Zone.Name].[All Zone.Names].[Czech Republic]":{"caption":"Czech Republic"},"[Zone.Name].[All Zone.Names].[Malta]":{"caption":"Malta"},"[Zone.Name].[All Zone.Names].[Lithuania]":{"caption":"Lithuania"},"[Zone.Name].[All Zone.Names].[Denmark]":{"caption":"Denmark"},"[Zone.Name].[All Zone.Names].[Estonia]":{"caption":"Estonia"},"[Zone.Name].[All Zone.Names].[The former Yugoslav Republic of Macedonia]":{"caption":"The former Yugoslav Republic of Macedonia"},"[Zone.Name].[All Zone.Names].[Hungary]":{"caption":"Hungary"},"[Zone.Name].[All Zone.Names].[Latvia]":{"caption":"Latvia"},"[Zone.Name].[All Zone.Names].[Germany]":{"caption":"Germany"},"[Zone.Name].[All Zone.Names].[Bulgaria]":{"caption":"Bulgaria"},"[Zone.Name].[All Zone.Names].[Sweden]":{"caption":"Sweden"},"[Zone.Name].[All Zone.Names].[Greece]":{"caption":"Greece"},"[Zone.Name].[All Zone.Names].[Netherlands]":{"caption":"Netherlands"},"[Zone.Name].[All Zone.Names].[Liechtenstein]":{"caption":"Liechtenstein"},"[Zone.Name].[All Zone.Names].[Ireland]":{"caption":"Ireland"},"[Zone.Name].[All Zone.Names].[Poland]":{"caption":"Poland"},"[Zone.Name].[All Zone.Names].[Spain]":{"caption":"Spain"}});
 }
-*/
+
 function test12(){
- var result = query.explore(["Traffic", "[Traffic]", "[Zone]", "[Zone.Name]", "[Zone.Name].[Name0]"], true);
+ var result = query.explore(["Traffic", "[Traffic]", "[Zone]", "[Zone.Name]", "[Zone.Name].[Name0]", "[Zone.Name].[France]"], true);
  var props = Object.keys(result);
- //equal(props.length, 2, "only error and data alright");
- //equal(result["error"], "OK", "no error");
- //notEqual(result["data"], null);
- //notEqual(result["data"], "Invalid identifier");
-equal(1,1);
+ equal(props.length, 2, "only error and data alright");
+ equal(result["error"], "OK", "no error");
+ notEqual(result["data"], null);
+ notEqual(result["data"], "Invalid identifier");
+ equal(1,2);
 }
-/*
+
 function test13(){
   var result = query.explore(["Traffic", "[Traffic]", "[Zone]", "[Zone.Name]", "[Zone.Name].[Name0]"], "wrong parameter");
   var props = Object.keys(result);
@@ -293,12 +293,12 @@ function testSliceRange(){
   }
 ]);
 }
-*/
+
 function runTest(f){
   test(f.toString(), f);
 }
 
-function runTests(){/*
+function runTests(){
   test(test1.toString(), test1);
   test(test2.toString(), test2);
   test(test3.toString(), test3);
@@ -309,11 +309,11 @@ function runTests(){/*
   test(test8.toString(), test8);
   test(test9.toString(), test9);
   test(test10.toString(), test10);
-  test(test11.toString(), test11);*/
+  test(test11.toString(), test11);
   test(test12.toString(), test12);
- /* test(test13.toString(), test13);
+  test(test13.toString(), test13);
   test(testClientRemark1.toString(), testClientRemark1);
-  test(testSliceRange.toString(), testSliceRange);*/
+  test(testSliceRange.toString(), testSliceRange);
 }
 
 runTests();
