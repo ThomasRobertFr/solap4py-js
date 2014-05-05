f7055b34f0319bdd2b2009b82a5089d85d443553
pierredepeyrot <pierredepeyrot@orange.fr>
2014-05-05 15:57:33 +0200

    added test12

--- Tests/TestEtape1.js
+++ Tests/TestEtape1.js
@@ -240,12 +240,11 @@ function test11(){
 }
 
 function test12(){
-/* var result = query.explore(["Traffic", "[Traffic]", "[Zone]", "[Zone.Name]", "[Zone.Name].[Name1]", "[Zone.Name].[France].[EST]"], true, 0);
+ var result = query.explore(["Traffic", "[Traffic]", "[Zone]", "[Zone.Name]", "[Zone.Name].[Name1]", "[Zone.Name].[France].[EST]"], true, 0);
  var props = Object.keys(result);
  equal(props.length, 2, "only error and data alright");
  equal(result["error"], "OK", "no error");
- notEqual(result["data"], null);*/
-equal("1", "2");
+ notEqual(result["data"], null);
 }
 
 function test13(){
